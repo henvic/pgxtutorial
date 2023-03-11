ebcdbed7d4841e5aad21997992a6f04fe5dc2e2a
Henrique Vicente <henriquevicente@gmail.com>
2023-03-11 20:08:02 +0100

    ci: Adding govulncheck.

--- scripts/ci-lint-install.sh
+++ scripts/ci-lint-install.sh
@@ -2,3 +2,4 @@ source scripts/lib.sh
 
 ensure_go_binary honnef.co/go/tools/cmd/staticcheck
 ensure_go_binary github.com/securego/gosec/v2/cmd/gosec
+ensure_go_binary golang.org/x/vuln/cmd/govulncheck
