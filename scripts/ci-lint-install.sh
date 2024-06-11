9f570e1ef05ffe3f0f4bb3aef10d2531094f0c68
Henrique Vicente <henriquevicente@gmail.com>
2024-06-11 04:28:18 +0200

    ci: update workflows

--- scripts/ci-lint-install.sh
+++ scripts/ci-lint-install.sh
@@ -1,5 +1,7 @@
 source scripts/lib.sh
 
+# TODO(henvic): Version these developer dependencies.
+
 ensure_go_binary honnef.co/go/tools/cmd/staticcheck
 ensure_go_binary github.com/securego/gosec/v2/cmd/gosec
 ensure_go_binary golang.org/x/vuln/cmd/govulncheck
